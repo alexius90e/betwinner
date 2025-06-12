6be26be56c3492360f2e2b1a21388e0ad323165c
Aliaksei Karzhou <alexius90e@gmail.com>
2025-06-12 14:20:22 +0300

    feat: added promocodes

--- js/index.js
+++ js/index.js
@@ -2,27 +2,18 @@ const installSwiperEl = document.querySelector('.install__slider .swiper');
 
 if (installSwiperEl) {
   const installSwiper = new Swiper(installSwiperEl, {
-    slidesPerView: 1,
+    slidesPerView: 'auto',
     spaceBetween: 16,
-    breakpoints: {
-      320: {
-        slidesPerView: 1.1,
-      },
-      577: {
-        slidesPerView: 1.5,
-      },
-      769: {
-        slidesPerView: 2,
-      },
-      1025: {
-        slidesPerView: 2.5,
-      },
-      1201: {
-        slidesPerView: 3,
-      },
-      1401: {
-        slidesPerView: 4,
-      },
-    },
   });
 }
+
+
+const promocodesSwiperEl = document.querySelector('.promocodes__slider .swiper');
+
+if (promocodesSwiperEl) {
+  const promocodesSwiper = new Swiper(promocodesSwiperEl, {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+  });
+}
+
